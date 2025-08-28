75eedc04afd3be889db4e742ebe5cb4242d77240
Abdoulkarim <17a.karim@gmail.com>
2025-08-28 19:00:22 +0100

    feat: add media query to ensure application is response on all screen sizes.
    Add star Component to represent rating which is selectable by user

--- src/components/movie-review-section/SelectRatings.jsx
+++ src/components/movie-review-section/SelectRatings.jsx
@@ -1,12 +1,35 @@
 import { Star } from "lucide-react";
+import { useState } from "react";
 
 function SelectRatings() {
+  const [hoverValue, setHoverValue] = useState("");
+  const [rating, setrating] = useState("");
+
+  const displayValue = hoverValue || rating;
+
   return (
     <div className="ratings-container">
-      {/* Outline star */}
-      <Star size={32} color="#f5c518" />
-      {/* Filled star */}
-      <Star size={32} color="#f5c518" fill="#f5c518" />
+      <div className="rating">
+        {Array.from({ length: 10 }).map((_, i) => (
+          <span
+            className="star-container"
+            onMouseEnter={() => setHoverValue(i + 1)}
+            onMouseLeave={() => setHoverValue(null)}
+            onClick={() => setrating(i + 1)}
+          >
+            <Star
+              key={i}
+              className="star"
+              size={26}
+              rating
+              color="#f5c518"
+              fill={i < displayValue ? "#f5c518" : "none"}
+            />
+          </span>
+        ))}
+      </div>
+
+      <p>{hoverValue || rating}</p>
     </div>
   );
 }
