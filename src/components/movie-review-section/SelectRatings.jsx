22c8ac4f0a6198a927c92490b69b89974d541bca
Abdoul Kourouma <Abdoul.Kourouma@croudace.co.uk>
2025-08-28 13:06:34 +0100

    imported star Component to allow users to rate a movie

--- src/components/movie-review-section/SelectRatings.jsx
+++ src/components/movie-review-section/SelectRatings.jsx
@@ -1,5 +1,14 @@
+import { Star } from "lucide-react";
+
 function SelectRatings() {
-  return <div className="ratings-container"></div>;
+  return (
+    <div className="ratings-container">
+      {/* Outline star */}
+      <Star size={32} color="#f5c518" />
+      {/* Filled star */}
+      <Star size={32} color="#f5c518" fill="#f5c518" />
+    </div>
+  );
 }
 
 export default SelectRatings;
