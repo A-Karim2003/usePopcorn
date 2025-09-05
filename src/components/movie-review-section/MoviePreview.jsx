92cbf6f6e484c6c763e8cdd21f012844fadfad1c
Abdoulkarim <17a.karim@gmail.com>
2025-09-05 19:29:40 +0100

    updated MoviePreview section to display data based on Selected movie

--- src/components/movie-review-section/MoviePreview.jsx
+++ src/components/movie-review-section/MoviePreview.jsx
@@ -3,24 +3,22 @@ import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
 
 import Button from "../Button";
 
-function MoviePreview({ setToggleBackbtn }) {
+function MoviePreview({ setToggleBackbtn, selectedMovie }) {
+  if (!selectedMovie) return;
   return (
     <div className="movie-details-container">
       <div className="img-container">
-        <img
-          src={
-            "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg"
-          }
-          alt={""}
-        />
+        <img src={selectedMovie.Poster} alt={selectedMovie.Title} />
       </div>
 
       <div className="movie-details">
-        <h3 className="movie-name">Inception</h3>
+        <h3 className="movie-name">{selectedMovie.Title}</h3>
         <div className="movie-detail-stats">
-          <p>16 Jul 2010 • 148 min</p>
-          <p>Action, Adventure, Sci-Fi</p>
-          <p>⭐️ 8.8 IMDb rating</p>
+          <p>
+            {selectedMovie.Released} • {selectedMovie.Runtime}
+          </p>
+          <p>{selectedMovie.Genre}</p>
+          <p>⭐️ {selectedMovie.imdbRating} IMDb rating</p>
         </div>
       </div>
 
